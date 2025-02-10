105f5b1d1790e8e82cd2da1661373744b1a170f9
ramazantuf <84492703+ramazantuf@users.noreply.github.com>
2025-02-10 15:46:55 +0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/data')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from MySQL:</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
